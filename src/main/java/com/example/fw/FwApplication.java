79d62389f3677891743b53a5e6400988acd58e1e
firedwood <woals2342@gmail.com>
2025-03-14 01:02:30 +0900

    Spring Boot 초기 세팅 완료

---
+++ src/main/java/com/example/fw/FwApplication.java
@@ -0,0 +1,12 @@
+package com.example.fw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FwApplication.class, args);
+    }
+}
